e61c0abbc47c1648c4cdb5607de1edc3a4bc0bb6
Ness Bautista <nestor.jbautista@gmail.com>
2015-08-28 12:35:58 -0500

    +Thread directory: Examples related to threading issues

---
+++ Threads/TimerActivation/TimerActivation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimerActivation
+//
+//  Created by Ness on 8/28/15.
+//  Copyright (c) 2015 Ness. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Threads/TimerActivation/TimerActivation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimerActivation
+//
+//  Created by Ness on 8/28/15.
+//  Copyright (c) 2015 Ness. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
