0ca8da990dbd643b5ca648e1862aab448e294114
Nestor Javier Hernandez Bautista <nestor.hernandez@definityfirst.com>
2017-04-29 11:41:34 -0500

    Addeda binary tree excercise

---
+++ DataStructsSwift/DataStructsSwift/blackboard.swift
@@ -0,0 +1,13 @@
+//
+//  blackboard.swift
+//  DataStructsSwift
+//
+//  Created by Nestor Javier Hernandez Bautista on 4/25/17.
+//  Copyright © 2017 Ness. All rights reserved.
+//
+
+import UIKit
+
+class blackboard: NSObject {
+
+}
---
+++ DataStructsSwift/Graph.swift
@@ -0,0 +1,13 @@
+//
+//  Graph.swift
+//  DataStructsSwift
+//
+//  Created by Nestor Javier Hernandez Bautista on 4/25/17.
+//  Copyright © 2017 Ness. All rights reserved.
+//
+
+import UIKit
+
+class Graph: NSObject {
+
+}
