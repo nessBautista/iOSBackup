c0979c20de278642307448c67666195e3ac3d9e9
Nestor Javier Hernandez Bautista <nestor.hernandez@definityfirst.com>
2017-04-29 13:00:32 -0500

    Sort insertion and quicsort

---
+++ DataStructsSwift/DataStructsSwift/Sort.swift
@@ -0,0 +1,13 @@
+//
+//  Sort.swift
+//  DataStructsSwift
+//
+//  Created by Nestor Javier Hernandez Bautista on 4/29/17.
+//  Copyright © 2017 Ness. All rights reserved.
+//
+
+import UIKit
+
+class Sort: NSObject {
+
+}
